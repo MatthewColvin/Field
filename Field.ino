0df10fdd4897c1122b7bf089338adec0fb437f0f
Matthew <mc067415@ohio.edu>
2023-07-12 21:36:54 -0500

    still have linker errors but this code path seem to be working see simpleRecieverWithCallback
    Change-Id: I102fcb6c0cf29b49a6bbad45ee553faa81895615

--- Field.ino
+++ Field.ino
@@ -2,53 +2,86 @@
 #include <IRremote.h>
 #include <string.h>
 
-#define IR_LED 4
-#define IR_DETECT 13
+#define IR_LED 5
+#define IR_DETECT 6
+#define STATUS_LED 7
 
 class IRTransciever
 {
 public:
+  static IRTransciever* getInstance(){
+    if(!mInstance){
+      return new IRTransciever(IR_LED,IR_DETECT);
+    }else {
+      return mInstance;
+    }
+  }
+  
   IRTransciever(int aTxPin, int aRxPin)
   {
     mIrSend.begin(aTxPin);
     mIrRec.begin(aRxPin);
+    mIrRec.registerReceiveCompleteCallback(&IRTransciever::ReceiveCompleteCallbackHandler);
+    mInstance = nullptr;
+    mIRDataJustReceived = false;
   }
 
   // Kinda guessing at the emplementation of this function.
   // may need to play with how we do this.
-  void send(uint16_t address, uint8_t data)
+  static void send(uint16_t address, uint16_t data)
   {
     // Send address and data no repeats
-    mIrSend.sendNEC(address, data, 0);
+    auto sendData = address | (data << 16);
+    mIrSend.sendNECMSB(sendData,32);
+    //mIrSend.sendNEC(address, data, 0);
   }
 
-  void handleRx()
+  static void handleRx()
   {
-    if (mIrRec.decode())
-    { // We have captured something.
-      // The capture has stopped at this point.
-      Serial.print("IR RX: Addr" + String(mIrRec.decodedIRData.address) + " Val:" + String(mIrRec.decodedIRData.command));
+    if (mIRDataJustReceived)
+    { 
+      mIrRec.printIRResultAsCVariables(&Serial);
+      mIRDataJustReceived = false;
+      mIrRec.resume();
     }
   }
 
-private:
-  IRsend mIrSend;
-  IRrecv mIrRec;
-};
+  static void ReceiveCompleteCallbackHandler() {
+    IrReceiver.decode(); // fill IrReceiver.decodedIRData
 
-IRTransciever ir(IR_LED, IR_DETECT);
+    if (IrReceiver.decodedIRData.command == 0x10) {
+        // do something SHORT here
+    } else if (IrReceiver.decodedIRData.command == 0x11) {
+        // do something SHORT here too
+    }
+
+    mIRDataJustReceived = true;
+}
+
+private:
+  static IRTransciever* mInstance;
+  static IRsend mIrSend;
+  static IRrecv mIrRec;
+  static volatile bool mIRDataJustReceived;
+
+};
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(STATUS_LED, OUTPUT);
+  digitalWrite(STATUS_LED, 0);
+  IRTransciever::getInstance();
 }
 
 uint16_t txAddr = 0;
 uint16_t txVal = 0;
 void loop()
 {
-  ir.handleRx();
-  ir.send(txAddr, txVal);
-  Serial.print("IR TX: Addr:" + String(txAddr) + "Val:" + String(txVal));
-  txVal++;
+  IRTransciever::getInstance()->handleRx();
+  //ir.send(txAddr, txVal);
+  //Serial.print("IR TX: Addr:" + String(txAddr) + " Val:" + String(txVal) + "\r\n");
+  //txVal++;
+  //digitalWrite(STATUS_LED, !digitalRead(STATUS_LED));
 }
+
