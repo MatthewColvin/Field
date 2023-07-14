0707009fe11f26ffdafc233fad975e23130298a6
Matthew Colvin <Matthew.Colvin@garmin.com>
2023-07-14 17:12:37 -0500

    partial commit
    Change-Id: Ie061912f3cfa0fd757d509fbbac0d98dd61a00d8

--- Field.ino
+++ Field.ino
@@ -3,36 +3,37 @@
 
 #include "IRTransciever.hpp"
 #include <Arduino.h>
+#include <ArduinoSTL.h>
 #include <string.h>
 
 #define STATUS_LED 7
 
-IRTransceiver *ir;
+IRChallenge *challenge;
+class IRChallenge {
+public:
+  IRChallenge(IRTransceiver *anIr) : mIr(anIr) {}
+
+  // Call in loop() do not block!!
+  void handle() { mIr->handleRx(); }
+
+  // Call to reset Challenge
+  void reset();
+
+private:
+  enum class Challenge { ReceiveData, PerformOperation };
+
+  void sendOperationToRover();
+  IRTransceiver *mIr = nullptr;
+};
 
 void setup() {
   Serial.begin(9600);
   pinMode(STATUS_LED, OUTPUT);
   digitalWrite(STATUS_LED, 0);
-
-  ir = IRTransceiver::getInstance();
+  challenge = new IRChallenge(IRTransceiver::getInstance());
 }
 
-class IRChallenge {
-public:
-  IRChallenge(IRTransceiver *ir) {}
-
-  void updateChallengeStatus();
-
-private:
-  enum class Challenge { SendData, PerformOperation };
-
-  void sendOperationToRover();
-};
-
 void loop() {
+  challenge->handle();
   // Should not block will need to find a way to keep loop running.
-  IRTransceiver::getInstance()->handleRx();
-
-  // Serial.print("IR TX: Addr:" + String(txAddr) + " Val:" + String(txVal) +
-  // "\r\n"); txVal++; digitalWrite(STATUS_LED, !digitalRead(STATUS_LED));
 }
